16ef5e05069444a7a21358e35f0b0426e31173f9
RaphRiots <118174094+RaphRiots@users.noreply.github.com>
2022-12-06 20:32:59 +0100

    start

---
+++ assets/java/script.js
@@ -0,0 +1,32 @@
+var sessionLength = 25;
+var breakLength = 5;
+
+function addSessionTime() {
+  sessionLength += 1;
+  $('#timer-session').text(sessionLength);
+  $('#timeLeft').text(sessionLength);
+}
+
+function deduceSessionTime() {
+  if (sessionLength > 1) {
+    sessionLength -= 1;
+  } else {
+    sessionLength = 1;
+  }
+  $('#timer-session').text(sessionLength);
+  $('#timeLeft').text(sessionLength);
+}
+
+function deduceBreakTime() {
+  if (breakLength > 1) {
+    breakLength -= 1;
+  } else {
+    breakLength = 1;
+  }
+  $('#timer-break').text(breakLength);
+}
+
+function addBreakTime() {
+  breakLength += 1;
+  $('#timer-break').text(breakLength);
+}
